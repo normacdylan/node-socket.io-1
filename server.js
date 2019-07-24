83986640ff5e1453535b55a9888548e21430a0de
August Posner <august_posner@hotmail.com>
2019-07-24 14:17:04 +0200

    Send message from io

--- server.js
+++ server.js
@@ -22,15 +22,16 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-
-  io.on('data', function(data) {
-    socket.emit(`${data} was received`)
-  })
-  
+  socket.emit('hello')
 });
+
+io.on('data', function(data) {
+  io.emit('msg', data);
+})
 
 // io.on('data', function(data) {
 //   console.log('data received', data);
 // })
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
