0ac67d27e766cdf785c27e34f54051a12451ce61
August Posner <august_posner@hotmail.com>
2019-07-24 22:41:44 +0200

    fix misspell

--- server.js
+++ server.js
@@ -15,15 +15,12 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
-  var clientIpAddress = sock.request.headers['x-forwarded-for'] || sock.request.connection.remoteAddress;
+  var clientIpAddress = socket.request.headers['x-forwarded-for'] || socket.request.connection.remoteAddress;
   console.log(' new request from : '+clientIpAddress);
   socket.on('disconnect', () => console.log('Client disconnected'));
   io.emit('someone connected')
   socket.emit('you are connected')
-  io.emit('msg', `${socket.toString()} joined`)
-  // socket.use((packet, next) => {
-  //   console.log('Received something: ' + packet.toString());
-  // })
+
 });
 
 io.on('data', function(data) {
@@ -37,44 +34,3 @@ io.on("*", function(input) {
 io.on('data', function(data) {
   console.log('data received', data);
 })
-
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-// var net = require('net');
-
-// const port = process.env.PORT || 3000
-
-// var server = net.createServer(function(socket) {
-// 	socket.write('Echo server\r\n');
-// 	socket.pipe(socket);
-// });
-
-// // server.listen(3000, 'https://powerful-garden-58783.herokuapp.com/');
-// // server.listen(port, '34.202.247.40');
-// server.listen(port);
-
-// 'use strict';
-
-// const express = require('express');
-// const WebSocketServer = require('ws').Server;
-// const path = require('path');
-
-// const PORT = process.env.PORT || 3000;
-// const INDEX = path.join(__dirname, 'index.html');
-
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX) )
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-// const wss = new WebSocketServer({ server });
-
-// wss.on('connection', (ws) => {
-//   console.log('Client connected');
-//   ws.on('close', () => console.log('Client disconnected'));
-// });
-
-// setInterval(() => {
-//   wss.clients.forEach((client) => {
-//     client.send(new Date().toTimeString());
-//   });
-// }, 1000)
