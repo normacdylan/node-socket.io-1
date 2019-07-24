35441cd0e63369620639caacac3279e852bfdb46
August Posner <august_posner@hotmail.com>
2019-07-24 16:18:01 +0200

    Go back to socket io

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-// 'use strict';
+'use strict';
 
-// const express = require('express');
-// const socketIO = require('socket.io');
-// const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-// const PORT = process.env.PORT || 3000;
-// const INDEX = path.join(__dirname, 'index.html');
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX) )
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// const io = socketIO(server);
+const io = socketIO(server);
 
-// io.on('connection', (socket) => {
-//   console.log('Client connected');
-//   socket.on('disconnect', () => console.log('Client disconnected'));
-//   io.emit('someone connected')
-//   socket.emit('you are connected')
-//   io.emit('msg', `${socket.toString()} joined`)
-// });
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  io.emit('someone connected')
+  socket.emit('you are connected')
+  io.emit('msg', `${socket.toString()} joined`)
+});
 
-// io.on('data', function(data) {
-//   io.emit('msg', data);
-// })
+io.on('data', function(data) {
+  io.emit('msg', data);
+})
 
-// io.on("*", function(input) {
-//   io.emit('msg', input.toString())
-// })
+io.on("*", function(input) {
+  io.emit('msg', input.toString())
+})
 
-// io.on('data', function(data) {
-//   console.log('data received', data);
-// })
+io.on('data', function(data) {
+  console.log('data received', data);
+})
 
 // setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
@@ -48,28 +48,28 @@
 // // server.listen(port, '34.202.247.40');
 // server.listen(port);
 
-'use strict';
+// 'use strict';
 
-const express = require('express');
-const WebSocketServer = require('ws').Server;
-const path = require('path');
+// const express = require('express');
+// const WebSocketServer = require('ws').Server;
+// const path = require('path');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+// const PORT = process.env.PORT || 3000;
+// const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX) )
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const wss = new WebSocketServer({ server });
+// const wss = new WebSocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
+//   ws.on('close', () => console.log('Client disconnected'));
+// });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000)
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000)
