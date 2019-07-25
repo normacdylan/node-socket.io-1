663aa0c708834114c207769ed6b2cb50c19f29cc
August Posner <august_posner@hotmail.com>
2019-07-25 09:50:49 +0200

    Change to net

--- server.js
+++ server.js
@@ -1,43 +1,58 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+// const express = require('express');
+// const socketIO = require('socket.io');
+// const path = require('path');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+// const PORT = process.env.PORT || 3000;
+// const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX) )
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
-var middleware = require('socketio-wildcard')();
+// const io = socketIO(server);
+// var middleware = require('socketio-wildcard')();
 
-io.use(middleware)
+// io.use(middleware)
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  var clientIpAddress = socket.request.headers['x-forwarded-for'] || socket.request.connection.remoteAddress;
-  console.log(' new request from : '+clientIpAddress);
-  socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on('*', function(packet){
-    console.log('packet received', packet);
-    if (packet.data) {
-      console.log('packet data', packet.data);
-    }
-  });
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   var clientIpAddress = socket.request.headers['x-forwarded-for'] || socket.request.connection.remoteAddress;
+//   console.log(' new request from : '+clientIpAddress);
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+//   socket.on('*', function(packet){
+//     console.log('packet received', packet);
+//     if (packet.data) {
+//       console.log('packet data', packet.data);
+//     }
+//   });
+// });
+
+// io.on('data', function(data) {
+//   console.log('received data:' + data);
+//   io.emit('msg', data);
+// })
+
+// io.on("*", function(input) {
+//   io.emit('msg', input.toString())
+// })
+
+// io.on('data', function(data) {
+//   console.log('data received', data);
+// })
+
+var net = require('net');
+
+const port = process.env.PORT || 3000;
+
+var server = net.createServer(function(socket) {
+  console.log('connection made')
+	socket.write('Echo server\r\n');
+  socket.pipe(socket);
+  socket.on('data', function(data) {
+    console.log('received data: ' + data);
+  })
 });
 
-io.on('data', function(data) {
-  console.log('received data:' + data);
-  io.emit('msg', data);
-})
-
-io.on("*", function(input) {
-  io.emit('msg', input.toString())
-})
-
-io.on('data', function(data) {
-  console.log('data received', data);
-})
+server.listen(port);
